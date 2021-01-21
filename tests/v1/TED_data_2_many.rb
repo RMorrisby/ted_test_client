39f9d5f0937e13755d89332caec17c7f99cca47b
Richard Morrisby <rmorrisby@gmail.com>
2021-01-21 14:46:38 +0000

    Refactored test to send results with varying statuses

--- tests/v1/TED_data_2_many.rb
+++ tests/v1/TED_data_2_many.rb
@@ -34,59 +34,74 @@ class TED_DATA_2_MANY < Test::Unit::TestCase
 
     # We will cheat here & send directly to TED
     
-    count = 4 # TODO read count from commandline
+    # count = 4 # TODO read count from commandline
 
-    count.times do |i|
-      # First, we will need to register the test
-      
-      # Set these @@ then call register_test_with_ted
+    # count.times do |i|
+    #   send_to_ted(i)      
+    # end # end count
 
-      @test_name = self.to_s.split("(")[0] + "_" + (i + 1).to_s
-      ted_test = TEDTest.new
-      ted_test.name = @test_name
-      ted_test.dir = @@test_file_dir
-      ted_test.priority = @@priority
-      ted_test.categories = @@categories.join("|")
-      ted_test.description = @@description
-      ted_test.notes = @@notes
-      ted_test.owner = @@owner
-      
-      register_test_with_ted
+    statuses = %w{ PASSED FAILED }
 
+    suffix = "always_pass"
+    status = "PASSED"
+    send_to_ted(suffix, status)
 
-      ted_result = TEDResult.new
-      ted_result.suite = TedClientConfig::DEFAULT_SUITE_NAME
-      ted_result.name = self.to_s.split("(")[0] + "_" + (i + 1).to_s
-      ted_result.test_run = @@version
-      ted_result.status = "PASSED" # TODO randomise this?
-      ted_result.start_timestamp = (Time.now - 5).strftime("%Y-%m-%d %H:%M:%S")
-      ted_result.end_timestamp = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-      ted_result.ran_by = TedClientConfig::DEFAULT_OWNER
-      # ted_result.message = ""
+    suffix = "always_fail"
+    status = "PASSED"
+    send_to_ted(suffix, status)
 
-      puts ted_result.to_s
+    suffix = "pass_or_fail"
+    status = statuses.random
+    send_to_ted(suffix, status)
 
-      url = TedClientConfig::SERVER[:ted_url_send_in_result]
-      puts "Now sending POST to #{url}"
-      # resp = RestClient.post(url, ted_result.to_json)
-      
-      # resource = RestClient::Resource.new(
-      #     url,
-      #     verify_ssl: false
-      #   )
-        
-      #   resp = resource.post(ted_result.to_json)
+    suffix = "maybe_not_run"
+    # 50% of the time, don't run this test
+    status = ((rand(10) % 2 == 0) ? statuses.random : nil)
+    puts "Status was decided to be nil; will not send to TED" if status == nil
+    send_to_ted(suffix, status)
 
-      
-        resp = RestClient::Request.execute(method: :post, url: url,
-          payload: ted_result.to_json,
-          verify_ssl: false)
-
-      puts resp.code
-      puts resp.body
-    end # end count
 
     @test_end_time = Time.now
   end
 
+  def send_to_ted(suffix, status)
+    # If the status is nil, we want to not send it to TED
+    puts "#{suffix} :: #{status}"
+    return unless status
+    
+    @test_name = self.to_s.split("(")[0] + "_" + suffix
+    ted_test = TEDTest.new
+    ted_test.name = @test_name
+    ted_test.dir = @@test_file_dir
+    ted_test.priority = @@priority
+    ted_test.categories = @@categories.join("|")
+    ted_test.description = @@description
+    ted_test.notes = @@notes
+    ted_test.owner = @@owner
+    
+    register_test_with_ted
+
+
+    ted_result = TEDResult.new
+    ted_result.suite = TedClientConfig::DEFAULT_SUITE_NAME
+    ted_result.name = self.to_s.split("(")[0] + "_" + suffix
+    ted_result.test_run = @@version
+    ted_result.status = status
+    ted_result.start_timestamp = (Time.now - 5).strftime("%Y-%m-%d %H:%M:%S")
+    ted_result.end_timestamp = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    ted_result.ran_by = TedClientConfig::DEFAULT_OWNER
+    # ted_result.message = ""
+
+    puts ted_result.to_s
+
+    url = TedClientConfig::SERVER[:ted_url_send_in_result]
+    puts "Now sending POST to #{url}"
+    
+      resp = RestClient::Request.execute(method: :post, url: url,
+        payload: ted_result.to_json,
+        verify_ssl: false)
+
+    puts resp.code
+    puts resp.body
+      end
 end
